277a7181e24ea2b22a279981c427da933a79d272
Your Name <your-email@example.com>
2025-09-09 15:59:07 +1200

    Initial Commit

---
+++ Aqua-Minder/Aqua_MinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aqua_MinderApp.swift
+//  Aqua-Minder
+//
+//  Created by Praveen Kumar on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Aqua_MinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
